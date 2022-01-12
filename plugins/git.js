9c015c4696e065d242b242ef4ee348c6dfe300db
ℂᎾℂᎾ Ѕℐℛ <96687367+cocosir007@users.noreply.github.com>
2022-01-12 21:27:37 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,17 +10,17 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
+*Creator number : wa.me/15098611924?text=Hi%20Ameer%20Suhail.%20*
 
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
+*YOUTUBE CHANNEL : *https://youtube.com/channel/UCOxp3D41xx1aB-M0Hv0Xi3A*
 
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
+*Githublink (Setup)  :    *https://github.com/cocosir007/COCOSIR*
 
 *Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
 
 *Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
 
-*Video For Setting Bot : https://youtu.be/_D4ZYuUSXjs* 
+*SUBSCRIBE : https://youtube.com/channel/UCOxp3D41xx1aB-M0Hv0Xi3A* 
 
 *For More Updates Subscribe The Channel ☝*
 `}) 
